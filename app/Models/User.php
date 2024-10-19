1ec6c6f304e0377adf257876ad5f60e883794aec
luis <luis.iit.sa@gmail.com>
2024-10-19 13:06:34 -0300

    Add CPF, is_admin and soft deletes to users table
    Added a 'cpf' column, 'is_admin' flag, and soft delete functionality to the users table. Updated the User model to utilize these new fields and added a method to check if a user is an admin.

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,19 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * @property boolean $is_admin
+ */
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    /** @use HasFactory<UserFactory> */
+    use HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +26,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'cpf',
+        'is_admin'
     ];
 
     /**
@@ -44,5 +51,13 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     *
+     */
+    public function isAdmin(): bool
+    {
+        return $this->is_admin;
     }
 }
