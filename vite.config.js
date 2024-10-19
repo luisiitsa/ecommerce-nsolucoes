8e62d571bfe7d52e489b3cd9c6ed4ec8b319db51
luis <luis.iit.sa@gmail.com>
2024-10-19 17:06:56 -0300

    Switch from Tailwind to Bootstrap and update Vite config
    Replaced TailwindCSS with Bootstrap for styling and updated Vite config for external access. Also added necessary dependencies for Bootstrap in package.json.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',  // Permite que o Vite seja acessível externamente
+        hmr: {
+            host: 'nsolucoes.test', // URL do seu domínio configurado
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
