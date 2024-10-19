338afd291bff9f228453f8785ed670ac3c0e8976
luis <luis.iit.sa@gmail.com>
2024-10-19 13:19:30 -0300

    Add AdminSeeder and update database seeding
    Introduced AdminSeeder to create an admin user during database seeding. Updated UserFactory to include a 'cpf' field. Modified DatabaseSeeder to call AdminSeeder, replacing individual user creation.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'cpf' => fake()->numerify('###########'),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
