b0eb1f66bbedc558eb0d454b66c571c95b03f6e4
luis <luis.iit.sa@gmail.com>
2024-10-20 18:36:16 -0300

    Add registration form and login modal
    Implemented a new registration form view and linked it to a new route. Enhanced the navbar to show login and user profile options, with a modal for user login.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,33 +9,93 @@
 
     <!-- Inclui os assets do Bootstrap e outros arquivos via Vite -->
     @vite(['resources/js/app.js'])
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{ url('/') }}">Minha Loja</a>
+        <a class="navbar-brand" href="{{ route('app.home') }}">Minha Loja</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        {{--        <div class="collapse navbar-collapse" id="navbarNav">--}}
-        {{--            <ul class="navbar-nav ms-auto">--}}
-        {{--                <li class="nav-item">--}}
-        {{--                    <a class="nav-link active" href="{{ url('/') }}">Home</a>--}}
-        {{--                </li>--}}
-        {{--                <li class="nav-item">--}}
-        {{--                    <a class="nav-link" href="#">Produtos</a>--}}
-        {{--                </li>--}}
-        {{--                <li class="nav-item">--}}
-        {{--                    <a class="nav-link" href="#">Carrinho</a>--}}
-        {{--                </li>--}}
-        {{--                <li class="nav-item">--}}
-        {{--                    <a class="nav-link" href="#">Contato</a>--}}
-        {{--                </li>--}}
-        {{--            </ul>--}}
-        {{--        </div>--}}
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('app.home') }}">Produtos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Carrinho</a>
+                </li>
+
+                <!-- Verifica se o usuário está logado -->
+                @guest
+                    <!-- Caso o usuário não esteja logado -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">
+                            <i class="bi bi-person-circle"></i> Logar
+                        </a>
+                    </li>
+                @else
+                    <!-- Caso o usuário esteja logado -->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                           data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi bi-person-circle"></i> {{ explode(' ', Auth::user()->name)[0] }}
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+                            <li><a class="dropdown-item" href="{{ route('profile.edit') }}">Editar Perfil</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    Sair
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endguest
+            </ul>
+        </div>
     </div>
 </nav>
+
+<!-- Modal de Login -->
+<div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="loginModalLabel">Login</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <!-- Formulário de Login -->
+                <form method="POST" action="">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="email" name="email" required autofocus>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Senha</label>
+                        <input type="password" class="form-control" id="password" name="password" required>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <button type="submit" class="btn btn-primary">Logar</button>
+                        <a href="{{ route('app.register') }}" class="link-primary">Criar conta</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <div class="container mt-5">
     @yield('content')
--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,12 @@ Route::get('/', function () {
     $products = Product::all();
 
     return view('app.home', ['products' => $products]);
-});
+})->name('app.home');
 
 Route::get('/sales/{product}', function (Product $product) {
     return view('app.sale', compact('product'));
 })->name('app.sales.product');
+
+Route::get('/register', function () {
+    return view('app.register');
+})->name('app.register');
